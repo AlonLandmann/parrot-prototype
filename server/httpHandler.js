cabed59dd9645e64686d661456f2250a3e5bed4b
AlonLandmann <rafael.landmann@gmail.com>
2024-12-06 15:09:50 +0800

    add the correct status code for method not allowed

--- server/httpHandler.js
+++ server/httpHandler.js
@@ -5,7 +5,7 @@ export default function httpHandler(method, handler) {
         return await handler(req, res);
       }
       console.log('Invalid request method.');
-      return res.status(400).json({ success: false, message: 'Invalid request received.' });
+      return res.status(405).json({ success: false, message: 'Invalid request received.' });
     } catch (error) {
       console.log(error);
       return res.status(500).json({ success: false, message: 'Internal server error.' });
