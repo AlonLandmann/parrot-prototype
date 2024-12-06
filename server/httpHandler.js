87e8fc8681fea3e6e1bd3c50e3a1bf52f8efe197
AlonLandmann <rafael.landmann@gmail.com>
2024-12-06 14:49:00 +0800

    add wrapper for http handlers

---
+++ server/httpHandler.js
@@ -0,0 +1,14 @@
+export default function httpHandler(method, handler) {
+  return async (req, res) => {
+    try {
+      if (req.method === method) {
+        return await handler(req, res);
+      }
+      console.log('Invalid request method.');
+      return res.status(400).json({ success: false, message: 'Invalid request received.' });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ success: false, message: 'Internal server error.' });
+    }
+  }
+}
